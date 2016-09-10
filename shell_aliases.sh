8e52ac1721a883f84cbc735ad2e6f78167927a6c
Paul Somers <paulsomers@gmail.com>
2016-09-10 12:30:22 +0100

    Add some shell aliases

---
+++ shell_aliases.sh
@@ -0,0 +1,10 @@
+alias g=git
+alias esfix="eslint --fix --"
+alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
+alias localip="ipconfig getifaddr en0"
+alias listip="ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'"
+
+# Mac
+
+alias afk="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
+
